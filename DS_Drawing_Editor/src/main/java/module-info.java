344c997a4222fea068819748169d433e727654fd
yuyoung <hyyy8810@gmail.com>
2024-12-17 12:14:09 +0900

    패키지 수정했다 얼배야

--- DS_Drawing_Editor/src/main/java/module-info.java
+++ DS_Drawing_Editor/src/main/java/module-info.java
@@ -4,8 +4,8 @@ module com.example.ds_drawing_editor {
     requires javafx.web;
     requires java.desktop;
 
-    opens com.example.ds_drawing_editor to javafx.fxml;
-    exports com.example.ds_drawing_editor;
-    exports com.example.ds_drawing_editor.Controller;
-    opens com.example.ds_drawing_editor.Controller to javafx.fxml;
+    opens DS.teamproject.DrawingEditor to javafx.fxml;
+    exports DS.teamproject.DrawingEditor;
+    exports DS.teamproject.DrawingEditor.Controller;
+    opens DS.teamproject.DrawingEditor.Controller to javafx.fxml;
 }
